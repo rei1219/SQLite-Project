3a9537f9b84a1309d8b2eb83a2702d8316091c97
rei1219 <lilith0696@gmail.com>
2026-09-25 14:03:21 +0200

    Fixed a fix

--- src/sql.cpp
+++ src/sql.cpp
@@ -161,7 +161,9 @@ bool SQL_update_char(sqlite3 *db)
 	sqlite3_bind_int(stmt, 2, 4);
 	sqlite3_bind_text(stmt, 2, "UTAU/SynthV2", -1, SQLITE_STATIC);
 	sqlite3_bind_int(stmt, 3, 4);
-	
+	sqlite3_step(stmt);
+	sqlite3_reset(stmt);
+
 	sqlite3_bind_text(stmt, 1, "Megpoid Gumi", -1, SQLITE_STATIC);
 	sqlite3_bind_int(stmt, 2, 5);
 	sqlite3_bind_text(stmt, 2, "SynthV2", -1, SQLITE_STATIC);
@@ -175,6 +177,8 @@ bool SQL_update_char(sqlite3 *db)
 	std::cout << "Rows Updated: "
 			  << sqlite3_changes(db)
 			  << "\n";
+
+	sqlite3_finalize(stmt);
 
 	return 0;
 }
